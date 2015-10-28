38605f78beb1d1727117f7570ac8144b4a5fe9b9
John Dinsmore <djibba22@fullsail.edu>
2015-10-27 21:15:36 -0400

    installed angular material framework using bower

---
+++ public/js/controllers/HomeCtrl.js
@@ -0,0 +1,22 @@
+angular.module('myapp')
+.controller('HomeCtrl', ['$scope' , '$http', "$firebaseArray",function ($scope,$http,$firebaseArray) {
+	
+		var ref= new Firebase("https://wormburnerapp.firebaseio.com/");
+		$scope.artists = $firebaseArray(ref);
+		
+		$scope.postComment = function (){
+			$scope.artists.$add({
+				author: $scope.newComment.author,
+				body: $scope.newComment.body,
+				// scope.commentForm.$setPristine();
+			})
+		}
+
+		$scope.removeComment  = function(obj){
+
+			$scope.artists.$remove(obj).then(function  (ref) {
+				ref.key() === obj.$id;
+
+			});
+		}
+}]);
