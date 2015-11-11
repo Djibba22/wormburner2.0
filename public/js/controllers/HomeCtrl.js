dc20164717634922121bd0b92bbbb0859f10409f
John Dinsmore <djibba22@fullsail.edu>
2015-11-11 18:20:04 -0500

    I have the yelp API working with geolocation from the browser

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -18,21 +18,6 @@ app.controller('HomeCtrl', ['$scope' , '$http', '$firebaseArray',function ($scop
 
 			});
 		}
-		var x = document.getElementById("demo");
-		$scope.getLocation = function() {
-		    if (navigator.geolocation) {
-		        navigator.geolocation.getCurrentPosition($scope.showPosition);
-		    } else {
-		        x.innerHTML = "Geolocation is not supported by this browser.";
-		    }
-		}
-		$scope.showPosition = function(position) {
-		    x.innerHTML = "Latitude: " + position.coords.latitude + 
-		    "<br>Longitude: " + position.coords.longitude;
-		    var latlon = position.coords.latitude + "," + position.coords.longitude;
-		    var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false";
-
-    		document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>"; 
-		}
+		
 		
 }]);
--- public/js/controllers/YelpCtrl.js
+++ public/js/controllers/YelpCtrl.js
@@ -1,7 +1,62 @@
 app.controller('YelpCtrl', ['$scope' , '$http', '$firebaseArray',function ($scope,$http,$firebaseArray) {
-		$http({method: 'GET', url: '/js/coursedata.json'}).success(function(courseData){
-		$scope.dealsData = dealsData; // response data 
-		console.log(dealsData.length);
-		$scope.results = dealsData.length
-		});
+
+
+		
+		   
+	    $scope.getGolfDeals = function() {
+
+	    	$http({
+			  method: 'GET',
+			  url: 'http://ipinfo.io'
+			}).then(function successCallback(response) {
+				var latlong = response.data.loc;
+				var city = response.data.city;
+				console.log(response.data.city);
+			    console.log(response.data.loc);
+			    getGolf(city,latlong)
+			  }, function errorCallback(response) {
+			    console.log("no juice today");
+			  });
+	    	
+
+				// get a random string for oath nonce value
+					var randomString = function(length, chars) {
+					var result = {};
+						for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+										  return result;
+
+				}
+	    	         
+	      var getGolf = function(city,latlong) {
+	      	 var method = 'GET',
+	        url = 'https://api.yelp.com/v2/search/',
+	        params = {
+	          location: city,
+	          cll: latlong,
+	          callback: 'angular.callbacks._0',
+	          oauth_consumer_key: 'JHSRzBiAeTPWxZ5pS1dDTg',
+	          oauth_token: 'KdyMmpETKqPQY1VuA1gcXFqVu4F5yuCu',
+	          oauth_signature_method: 'HMAC-SHA1',
+	          oauth_timestamp:  new Date().getTime(),
+	          oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzA'),
+	          term: 'golf'
+	          
+	        },
+	        consumerSecret = 'b5tb39Cqft0Gromguo49PAmD79I',
+	        tokenSecret ='OsH_O_RxgJ-1NIQXviA5FaMMVqw',
+	        signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, {encodeSignature: false});
+	        params.oauth_signature = signature;
+	        $http.jsonp(url, {params: params}).then(function(callback){
+	        		console.log(callback);
+	        },function(error){
+	        		console.log(error);
+	        });
+	      }
+	       
+	   
+	  }
 }]);
+
+
+
+
