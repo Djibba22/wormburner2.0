71424e3e25f99f8fb6efd0524aeb81e4d5d3136d
John Dinsmore <djibba22@fullsail.edu>
2016-02-21 21:10:30 -0500

    SportsRadar disabled the key, I have applied for a new key but it has a status of waiting, i may use images as well,placeholders this closes #13

--- public/js/controllers/SportRadarCtrl.js
+++ public/js/controllers/SportRadarCtrl.js
@@ -1,8 +1,9 @@
 app.controller('SportRadarCtrl', ['$scope' , '$http',function ($scope,$http ) {
   console.log("comeback charlie");
-  $http({method: 'GET', url: 'http(s)://api.sportradar.us/golf-t1/schema/tournament/leaderboard-v1.0.xsd?api_key=rwppn9ub7aqmde94djw23ggm'}).success(function(courseData){
-  $scope.pgaData = pgaData; // response data
-  console.log("tourinfo=" + pgaData.length);
+  $http({method: 'GET', url: 'http(s)://api.sportradar.us/golf-t1/schema/tournament/leaderboard-v1.0.xsd?api_key=rwppn9ub7aqmde94djw23ggm'}).success(function(sportsData){
+  $scope.sportsData = sportsData; // response data
+  console.log("Sportsradar" + sportsData.length);
   $scope.results = pgaData.length
   });
 }]);
+//The controller is on place but the key has a waiting status, the placeholders are there and I have chosen to get information on tour leaders, I may pull images as well.
