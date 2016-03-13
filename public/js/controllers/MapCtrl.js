18fa943c44db55ee7e05c1cdcf7f5a81c4ccd238
John Dinsmore <djibba22@fullsail.edu>
2016-03-12 20:13:47 -0500

    Styling changes and weather forecast closes #5

--- public/js/controllers/MapCtrl.js
+++ public/js/controllers/MapCtrl.js
@@ -3,6 +3,7 @@ app.controller("MapCtrl", function($scope, uiGmapGoogleMapApi) {
   $scope.map = { zoom: 12 };
   //Center Map on current location.
   //The map now centers on the users current location and is better size
+
   navigator.geolocation.getCurrentPosition(function(pos){
     $scope.map.center = {
       latitude: pos.coords.latitude,
